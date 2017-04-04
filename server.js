326fee2f3284f43eb389b681e828011db7c44d63
jarwino <jarwino@gmail.com>
2017-04-04 15:29:09 -0400

    port

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get('/getPostures', function (req, res) {
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3000, function () {
+  console.log('App is running!')
 })
